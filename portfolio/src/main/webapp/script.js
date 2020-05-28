908af6d7111328217fc1ac61a495f7ab70afe7b2
Chiamaka <chimnchuk@gmail.com>
2020-05-28 13:26:12 -0400

    test slideshow successful

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,3 +17,32 @@
 /**
  * Adds a random greeting to the page.
  */
+ 
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 3000;	// Time Between Switch
+	 
+// Image List
+images[0] = "http://lorempixel.com/400/200/animals";
+images[1] = "https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg";
+images[2] = "https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png";
+
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
+
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++; 
+	} else { 
+		// Reset Back To O
+		i = 0;
+	}
+
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload=changeImg;
